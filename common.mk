0e303b505597bd25e1d1636d370c0e234c5b6c4a
Marc Bourgoin <themard85@gmail.com>
2023-06-19 10:37:13 -0600

    sm8550-common: Switch to prebuilt health AIDL
    Change-Id: I737dee80ba00e792dd2d50ab74523d3b6cb43d67

--- common.mk
+++ common.mk
@@ -188,9 +188,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health@1.0.vendor \
+    android.hardware.health@2.1.vendor
 
 # HIDL
 PRODUCT_PACKAGES += \
