258e6558f983c542e26aaeb78426aae254de3d46
SGCMarkus <markusornik@gmail.com>
2023-12-20 20:31:43 +0100

    sm8550-common: Update from rtwo_cn-user 14 U1TR34.8-19-2 9bfa3-adc28 release-keys
    Change-Id: I572675f414c049099b8065ad47b75858fbd47a97

--- rootdir/bin/init.qti.qcv.sh
+++ rootdir/bin/init.qti.qcv.sh
@@ -12,13 +12,27 @@ setprop ro.vendor.qti.soc_id $soc_id
 
 # For chipsets in QCV family, convert soc_id to soc_name
 # and store it in ro.vendor.qti.soc_name.
-if [ "$soc_id" -eq 557 ]; then
+
+if [ "$soc_id" -eq 608 ]; then
+    setprop ro.vendor.qti.soc_name crow
+    setprop ro.vendor.qti.soc_model SM7550
+elif [ "$soc_id" -eq 557 ]; then
     setprop ro.vendor.qti.soc_name pineapple
     setprop ro.vendor.qti.soc_model SM8650
-elif [ "$soc_id" -eq 519 ] || [ "$soc_id" -eq 536 ] || [ "$soc_id" -eq 600 ] || [ "$soc_id" -eq 601 ]; then
+elif [ "$soc_id" -eq 519 ] || [ "$soc_id" -eq 536 ]; then
     setprop ro.vendor.qti.soc_name kalama
     setprop ro.vendor.qti.soc_model SM8550
     setprop ro.vendor.media_performance_class 33
+elif [ "$soc_id" -eq 600 ] || [ "$soc_id" -eq 601 ]; then
+    setprop ro.vendor.qti.soc_name kalama
+    setprop ro.vendor.qti.soc_model SG8275
+    setprop ro.vendor.media_performance_class 33
+elif [ "$soc_id" -eq 603 ]; then
+    setprop ro.vendor.qti.soc_name kalama
+    setprop ro.vendor.qti.soc_model QCS8550
+elif [ "$soc_id" -eq 604 ]; then
+    setprop ro.vendor.qti.soc_name kalama
+    setprop ro.vendor.qti.soc_model QCM8550
 elif [ "$soc_id" -eq 457 ] || [ "$soc_id" -eq 482 ]; then
     setprop ro.vendor.qti.soc_name taro
     setprop ro.vendor.qti.soc_model SM8450
