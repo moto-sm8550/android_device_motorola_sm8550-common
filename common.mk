8974cbf9b35a14f6c26e80a3615af11fdc05ace3
Nick Reuter <nreuter85@gmail.com>
2026-07-02 22:20:04 -0500

    sm8550-common: Update Lineage Health HAL features
    Change-Id: I8705c19e149d252680b6ffc21e2c131f5a4ade6f

--- common.mk
+++ common.mk
@@ -168,10 +168,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/device/force_charging_disable)
-$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/module/qpnp_adaptive_charge/parameters/charging_enabled)
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
